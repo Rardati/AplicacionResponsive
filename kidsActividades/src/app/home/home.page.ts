18387ea6a6b4e16ce9f219aea1c04cba82ac2a47
rardati601 <rardati601@g.educaand.es>
2024-12-01 13:09:12 +0100

    Actualizar

--- kidsActividades/src/app/home/home.page.ts
+++ kidsActividades/src/app/home/home.page.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { HttpClient  } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-home',
@@ -7,16 +7,27 @@ import { HttpClient  } from '@angular/common/http';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage implements OnInit {
-  actividades: any[] = []; // Variable para almacenar los datos
+  actividades: any[] = []; // Almacena las actividades
+  filterText: string = ''; // Texto de búsqueda
 
   constructor(private http: HttpClient) {}
 
   ngOnInit() {
     // Cargar datos desde el archivo JSON remoto
     this.http
-      .get<any[]>('https://github.com/Rardati/AplicacionResponsive/blob/main/actividades.json')
+      .get<any[]>('https://raw.githubusercontent.com/Rardati/AplicacionResponsive/main/actividades.json') // URL Raw
       .subscribe((data) => {
-        this.actividades = data;
+        this.actividades = data; // Asignar datos al array actividades
       });
+  }
+
+  // Método para filtrar actividades basado en el texto de búsqueda
+  filterActivities() {
+    if (!this.filterText) {
+      return this.actividades; // Retornar todas las actividades si no hay filtro
+    }
+    return this.actividades.filter((actividad) =>
+      actividad.name.toLowerCase().includes(this.filterText.toLowerCase())
+    );
   }
 }
