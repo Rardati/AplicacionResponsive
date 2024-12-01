6e5bb286b308d60dac9c4b03434cb4df6cc05301
rardati601 <rardati601@g.educaand.es>
2024-12-01 17:18:07 +0100

    TodoModificado

--- kidsActividades/src/app/app.module.ts
+++ kidsActividades/src/app/app.module.ts
@@ -2,7 +2,8 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
-import { HttpClientModule } from '@angular/common/http'; // Asegúrate de importar HttpClientModule.
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms'; // Importar FormsModule
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
@@ -13,8 +14,8 @@ import { AppRoutingModule } from './app-routing.module';
     BrowserModule,
     IonicModule.forRoot(),
     AppRoutingModule,
-    HttpClientModule 
-
+    HttpClientModule,
+    FormsModule // Agregar FormsModule
   ],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
--- kidsActividades/src/app/home/home.page.ts
+++ kidsActividades/src/app/home/home.page.ts
@@ -1,23 +1,52 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import {map} from 'rxjs/operators';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage{
-
-  actividades: any; 
+export class HomePage implements OnInit {
+  searchText: string = '';
+  allActivities: any[] = []; // Todas las actividades
+  displayedActivities: any[] = []; // Actividades filtradas
 
   constructor(private httpClient: HttpClient) {}
 
   ngOnInit() {
-    // Cargar datos desde el archivo JSON remoto
-    this.actividades=  this.httpClient.get('https://raw.githubusercontent.com/Rardati/AplicacionResponsive/main/actividades.json').pipe(map((res:any) => res['results']));
-     
+    this.httpClient
+      .get('https://github.com/Rardati/AplicacionResponsive/blob/main/actividades.json')
+      .subscribe((data: any[]) => {
+        this.allActivities = data; // Guardar todas las actividades
+        this.displayedActivities = [...data]; // Mostrar todas inicialmente
+      });
   }
 
-  
+  // Filtrar actividades por texto ingresado
+  filterItems() {
+    const searchTextLower = this.searchText.toLowerCase();
+    this.displayedActivities = this.allActivities.filter((actividad) =>
+      actividad.nombre.toLowerCase().includes(searchTextLower)
+    );
+  }
+
+  // Obtener clase CSS según la categoría
+  getCategoryClass(category: string): string {
+    switch (category) {
+      case 'Manualidades':
+        return 'manualidades';
+      case 'Juegos al aire libre':
+        return 'juegos-al-aire-libre';
+      case 'Juegos tranquilos':
+        return 'juegos-tranquilos';
+      case 'Creatividad':
+        return 'creatividad';
+      case 'Educativo':
+        return 'educativo';
+      case 'Actividades físicas':
+        return 'actividades-fisicas';
+      default:
+        return '';
+    }
+  }
 }
