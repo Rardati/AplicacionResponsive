9399b739cd3f4f4a5c0cfaa6b042f32da5e8e087
rardati601 <rardati601@g.educaand.es>
2024-12-01 12:30:17 +0100

    Guardar

--- kidsActividades/src/app/home/home.page.ts
+++ kidsActividades/src/app/home/home.page.ts
@@ -1,12 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient  } from '@angular/common/http';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
+  actividades: any[] = []; // Variable para almacenar los datos
 
-  constructor() {}
+  constructor(private http: HttpClient) {}
 
+  ngOnInit() {
+    // Cargar datos desde el archivo JSON remoto
+    this.http
+      .get<any[]>('https://github.com/Rardati/AplicacionResponsive/blob/main/actividades.json')
+      .subscribe((data) => {
+        this.actividades = data;
+      });
+  }
 }
