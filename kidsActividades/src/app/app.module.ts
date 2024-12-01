01fde24044980e69add91784cd00b6f50f5d4c78
rardati601 <rardati601@g.educaand.es>
2024-12-01 16:13:21 +0100

    Actualizar

--- kidsActividades/src/app/app.module.ts
+++ kidsActividades/src/app/app.module.ts
@@ -2,10 +2,10 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
+import { HttpClientModule } from '@angular/common/http'; // Asegúrate de importar HttpClientModule.
+
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
-import { HttpClient } from '@angular/common/http';
-import { FormsModule } from '@angular/forms'; // Importar FormsModule
 
 @NgModule({
   declarations: [AppComponent],
@@ -13,8 +13,8 @@ import { FormsModule } from '@angular/forms'; // Importar FormsModule
     BrowserModule,
     IonicModule.forRoot(),
     AppRoutingModule,
-    HttpClient,
-    FormsModule, // Agregar FormsModule aquí
+    HttpClientModule 
+
   ],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
